4dc7b3114422bb3d689d92e24017dc2edaad2e02
hanppy123 <31874765+123weblike@users.noreply.github.com>
2018-01-25 20:25:11 +0800

    es6

---
+++ es6汇总/src/temp.js
@@ -0,0 +1,20 @@
+//1.export
+/*
+var name="zhangfan";
+var d="zf";
+var a="zh";
+/!*
+export function add(a,b){
+    return a+b;
+}*!/
+export {
+    name as name,
+    cname as d,
+    skill as a
+}
+*/
+
+//export可以写多个
+//export default只能写一个
+//2.export default
+export default var a='zhangfan';
